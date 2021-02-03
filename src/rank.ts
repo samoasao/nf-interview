6752b509b4126e511256a2c9831adce1f4f08067
samoasao <samoasao@gmail.com>
2021-02-03 11:53:07 -0700

    Testing Dictionary and Team object

--- src/rank.ts
+++ src/rank.ts
@@ -1,7 +1,14 @@
+import Team from './Team';
 
 export const rankTeams: (data: any) => Array<any> = (data) => {
-    console.log(data);
     //Create dictionary that stores teams
+    const teamDict: { [name: string]: Team } = {};
+
+    console.log(teamDict);
+
+    getTeam("Testing", teamDict);
+
+    console.log(teamDict);
 
     //Loop through data to tally scores store in dictionary
 
@@ -10,5 +17,12 @@ export const rankTeams: (data: any) => Array<any> = (data) => {
     //Sort array using custom compare function
     return ['return'];
 }
+
+const getTeam: (teamName: string, teamDict:{ [name: string]: Team }) => Team = (teamName, teamDict) => {
+    if (!teamDict[teamName]) {
+        teamDict[teamName] = new Team(teamName);
+    }
+    return teamDict[teamName];
+};
 
 export default rankTeams;
